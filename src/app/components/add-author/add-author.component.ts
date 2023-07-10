fc403818808a0222e7f5abf36684f808f1ccfc0e
mohhazem <moh.hazemhf@gmail.com>
2023-07-10 04:22:50 +0300

    fixed  forum

--- src/app/components/add-author/add-author.component.ts
+++ src/app/components/add-author/add-author.component.ts
@@ -1,20 +1,46 @@
 import { Component } from '@angular/core';
 import { author } from 'src/app/models/author';
+import { NetworkServicesService } from 'src/app/services/network-services.service';
 import { authors } from 'src/authors';
+
 @Component({
   selector: 'app-add-author',
   templateUrl: './add-author.component.html',
-  styleUrls: ['./add-author.component.css']
+  styleUrls: ['./add-author.component.css'],
+  providers:[NetworkServicesService]
 })
 export class AddAuthorComponent {
-  newAuthor:author={author: '', authorId:0,email:'',bio:'' ,createdAt:'',updatedAt:'' };
-
-  submitForm(newName:string,newEmail:string,newBio:string){
-    this.newAuthor.authorId=authors.length-1;
-    this.newAuthor.author=newName;
-    this.newAuthor.email=newEmail;
-    this.newAuthor.bio=newBio;
-    authors.push(this.newAuthor);
+  newAuthor: author = {
+    authorId: 0,
+    author: '',
+    email: '',
+    bio: '',
+    createdAt: '',
+    updatedAt: ''
+  };
+  service;
+  constructor(newService: NetworkServicesService) {
+    this.service = newService;
+  }
+  updateBio(newBio: string) {
+    console.log("zebyyyyy")
+    this.newAuthor.bio = newBio
+  }
+  updateAuthor(newName: string) {
+    console.log("555555")
+    this.newAuthor.author = newName;
+  }
+  updateEmail(newEmail: string) {
+    console.log("mangaaaa")
+    this.newAuthor.email = newEmail;
+  }
+  ngOnClick() {
+    console.log('a7aaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+    this.submitForm()
+  }
+  submitForm() {
+    this.newAuthor.authorId = authors.length ; 
+    this.service.postFake(this.newAuthor)
     console.log(authors);
   }
 }
--- src/app/services/network-services.service.ts
+++ src/app/services/network-services.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { book } from '../models/book';
 import { Observable } from 'rxjs';
+import { authors } from 'src/authors';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,5 +13,9 @@ export class NetworkServicesService {
   get():Observable<book[]> {
     console.log('Fetching data from server...');
     return this.http.get<book[]>(this.url);
+  }
+  postFake(newData:any){
+    console.log("entered api")
+    authors.push(newData)
   }
 }
